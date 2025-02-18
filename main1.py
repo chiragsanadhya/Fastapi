26d3db7f6c7941e50a90f01aa62f1bcd448f538b
chiragsanadhya <chiragsanadhya1411@gmail.com>
2025-02-18 14:14:45 +0530

    first commit

---
+++ main1.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get('/greet/{username}')
+async def greet(username:str):
+    return {"message" : f"Hello {username}"}
+
+
+@app.get('/')
+async def read_root():
+    return {"message" : "hello world"}
+
+#uvicorn main:app --reload
+#clrl + Z
+#pkill -9 Python
+
+
